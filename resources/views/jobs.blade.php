91c85b62596f783562468d73e1e4c4bf4ae64125
eman109 <ehusseinmmo@gmail.com>
2026-08-24 13:42:34 +0300

    debug-bar

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,13 +2,16 @@
     <x-slot:heading>
         Jobs Listing 
     </x-slot:heading>
-    <ul>
-        @foreach ( $jobs as $job )
-        <li>
-            <a href='/jobs/{{$job['id']}}'>
-                <strong>{{$job['title']}}:</strong> Pays {{$job['salary']}} per year.
+    <div class="space-y-4">
+        @foreach ($jobs as $job )
+            <a href='/jobs/{{$job['id']}}' class="block px-4 py-6 border border-gray-200">
+                <div class="font-bold text-blue-500">{{$job->employer->name}}</div>
+                <div>
+                    <strong>{{$job['title']}}:</strong> Pays {{$job['salary']}} per year.
+                </div>
             </a>
-        </li>
+            
         @endforeach
-    </ul> 
+
+    </div>
 </x-layout>
